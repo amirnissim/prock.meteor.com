0bc6e5eb6677eea2653a84f18785f782b75002f1
Amir Nissim <amirnissim@gmail.com>
2015-01-16 16:42:11 +0200

    added admin tools for db dump

--- tools/admin.sh
+++ tools/admin.sh
@@ -1,8 +1,8 @@
 # get a password for remote connection
 alias getpass='meteor mongo --url prock.meteor.com | cut -d: -f3 | cut -d@ -f1'
 
-alias db-export='mongoexport --collection events --csv --out export.csv --fields title,date,status,rsvps,walkins -u client -h production-db-a1.meteor.io:27017 -d prock_meteor_com -p `getpass`'
-alias db-dump='mongodump -u client -h production-db-a1.meteor.io:27017 -d prock_meteor_com -p `getpass`'
+alias db-export='mongoexport --collection events --csv --out export.csv --fields title,date,status,rsvps,walkins `coffee parse-mongo-url.coffee`'
+alias db-dump='mongodump `coffee parse-mongo-url.coffee`'
 
 # Mongo cheatsheet
 # db.events.update({_id:'id'},{$set: {maxParticipants:99}});
